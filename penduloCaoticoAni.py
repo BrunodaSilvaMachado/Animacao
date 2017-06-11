1e0bf38354f00281c0042e0ade383d6553e229f7
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2017-06-11 08:30:21 -0300

    'penduloCaotico-versao_final'

--- penduloCaoticoAni.py
+++ penduloCaoticoAni.py
@@ -39,22 +39,27 @@ class Pendulo(object):
 		self.v += (a_tmp+at)*dt/2.
 		self.e = 0.5*self.m*(self.l*self.v)**2 + (self.m*g*self.l*(1.-math.cos(self.x)))
 #end
-
-def grafico(visivel):
+		
+def grafico(title,visivel):
 	axes = plt.gca()
 	axes.axes.get_xaxis().set_visible(visivel)
+	axes.xaxis.grid(True)
+	axes.yaxis.grid(False)
 	axes.spines['top'].set_color('none')
 	axes.spines['right'].set_color('none')
 	axes.yaxis.set_ticks_position('left')
 	axes.xaxis.set_ticks_position('bottom')
 	axes.spines['bottom'].set_position(('data',0))
 	axes.spines['left'].set_position(('data',0))
+	plt.rc('text',usetex = True)
+	plt.rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
+	plt.ylabel(r'\raggedright{\textit{'+title+'}}')
 #end 
 
 #begin
 
-p1 = Pendulo(1.,10.,math.pi/6,0)
-p2 = Pendulo(1.,10.,math.pi/9,0)
+p1 = Pendulo(1.,10.,math.pi,0)
+p2 = Pendulo(1.,10.,0.1,0)
 
 tmax=20*p1.T
 t=np.arange(0,tmax,dt)
@@ -85,25 +90,25 @@ fig = plt.figure(facecolor='white')
 
 XxT = fig.add_subplot(321,xlim=(0,tmax),ylim=(-2,2))
 plt.setp(XxT.get_xticklabels(),visible = False)
-grafico(False)
-line1,=XxT.plot([],[],'k-',lw=2)
-line5,=XxT.plot([],[],'c-',lw=2)
+grafico('posi\c{c}\~{a}o',True)
+line1,=XxT.plot([],[],'g-',lw=2)
+line5,=XxT.plot([],[],'y-',lw=2)
 
 VxT = fig.add_subplot(323,xlim=(0,tmax),ylim=(-2,2))
 plt.setp(VxT.get_xticklabels(),visible = False)
-grafico(False)
-line2,=VxT.plot([],[],'m-',lw=2)
-line6,=VxT.plot([],[],'b-',lw=2)
+grafico('Velocidade',True)
+line2,=VxT.plot([],[],'g-',lw=2)
+line6,=VxT.plot([],[],'y-',lw=2)
 
 XxV = fig.add_subplot(122,xlim=(-np.pi,np.pi),ylim=(-2,2))
-grafico(False)
+grafico('X/V',True)
 line3,=XxV.plot([],[],'g.',lw=2)
 line7,=XxV.plot([],[],'y.',lw=1)
 
 ExT = fig.add_subplot(325,xlim=(min(t),max(t)),ylim=(min(e)-0.01,max(e)+0.01))
-grafico(True)
-line4,=ExT.plot([],[],'r-',lw=2)
-line8,=ExT.plot([],[],'b-',lw=2)
+grafico('Energia',True)
+line4,=ExT.plot([],[],'g-',lw=2)
+line8,=ExT.plot([],[],'y-',lw=2)
 
 
 #frame base
@@ -120,8 +125,8 @@ def init():
 
 #funcao animacao
 def animate(i):
-	a = t[:i]#np.linspace(0,2,1000)
-	b = x[:i]#np.sin(2 * np.pi * (x - 0.01*i))
+	a = t[:i]
+	b = x[:i]
 	c = v[:i]
 	d = e[:i]
 	f = t1[:i]
@@ -140,7 +145,7 @@ def animate(i):
 	return line1,line2,line3,line4,line5,line6,line7,line8,
 	
 #cria animacao
-anim = animation.FuncAnimation(fig,animate,init_func = init, frames=t.size,interval=0,blit=True)
+anim = animation.FuncAnimation(fig,animate,init_func = init, frames=t.size,interval=0,blit=True,repeat = False)
 
 plt.show()
 
